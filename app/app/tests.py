fc49f7d9ace06c9f815cfffebb1d76f294d00011
Ahmad Luqman <ahmad-luqman@users.noreply.github.com>
2022-12-20 13:09:19 +0500

    fix comments to fix diff

--- app/app/tests.py
+++ app/app/tests.py
@@ -7,16 +7,16 @@ from app import calc
 
 
 class CalcTests(SimpleTestCase):
-    """Tests the calc module"""
+    """Tests the calc module."""
 
     def test_add_numbers(self):
-        """Tests adding number together"""
+        """Test adding numbers together."""
         res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
 
     def test_subtract_numbers(self):
-        """Tests subtracting number together"""
+        """Test subtracting numbers."""
         res = calc.subtract(10, 15)
 
         self.assertEqual(res, 5)
