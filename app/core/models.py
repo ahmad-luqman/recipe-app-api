77970e9181f3dd2732ba6c357d7caa1085073ecb
Ahmad Luqman <ahmad-luqman@users.noreply.github.com>
2022-12-24 19:46:54 +0500

    fix more diff

--- app/core/models.py
+++ app/core/models.py
@@ -5,7 +5,7 @@ from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
     BaseUserManager,
-    PermissionsMixin
+    PermissionsMixin,
 )
 
 
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -6,7 +6,7 @@ from django.contrib.auth import get_user_model
 
 
 class ModelTests(TestCase):
-    """Tests for models."""
+    """Tests models."""
 
     def test_create_user_with_email_successful(self):
         """Test creating a user with an email is successful."""
