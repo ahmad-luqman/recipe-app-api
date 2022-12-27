48cc421fc7a5ec632ff1689b66a766c43ef23bb1
Ahmad Luqman <ahmad-luqman@users.noreply.github.com>
2022-12-27 12:37:03 +0500

    fix diff

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -24,7 +24,7 @@ class AdminSiteTests(TestCase):
             name='Test User',
         )
 
-    def test_users_list(self):
+    def test_user_list(self):
         """Test that users are listed on page."""
         url = reverse('admin:core_user_changelist')
         res = self.client.get(url)
