9b3c5495b48e0c8645449bca594ea3f19d848ee4
Ahmad Luqman <ahmad-luqman@users.noreply.github.com>
2022-12-27 12:37:55 +0500

    more diff fixed

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -8,7 +8,7 @@ from django.test import Client
 
 
 class AdminSiteTests(TestCase):
-    """Test for Django admin."""
+    """Tests for Django admin."""
 
     def setUp(self):
         """Create user and client."""
@@ -21,10 +21,10 @@ class AdminSiteTests(TestCase):
         self.user = get_user_model().objects.create_user(
             email='user@example.com',
             password='testpass123',
-            name='Test User',
+            name='Test User'
         )
 
-    def test_user_list(self):
+    def test_users_lists(self):
         """Test that users are listed on page."""
         url = reverse('admin:core_user_changelist')
         res = self.client.get(url)
