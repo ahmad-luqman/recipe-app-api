d05fa6680b5edf064a320b5b7104d9fa81ed5cb1
Ahmad Luqman <ahmad-luqman@users.noreply.github.com>
2022-12-28 12:09:36 +0500

    fix diff

--- app/app/urls.py
+++ app/app/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from drf_spectacular.views import (
     SpectacularAPIView,
-    SpectacularSwaggerView
+    SpectacularSwaggerView,
 )
+
 from django.contrib import admin
 from django.urls import path
 
@@ -26,6 +27,6 @@ urlpatterns = [
     path(
         'api/docs/',
         SpectacularSwaggerView.as_view(url_name='api-schema'),
-        name='api-docs'
-    ),
+        name='api-docs',
+    )
 ]
