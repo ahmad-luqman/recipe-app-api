407458faff8297d3137ce5d051549995cc9db762
Ahmad Luqman <ahmad-luqman@users.noreply.github.com>
2022-12-28 17:30:36 +0500

    fix diff

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -17,14 +17,3 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         """Create and return a user with encrypted password."""
         return get_user_model().objects.create_user(**validated_data)
-
-    def update(self, instance, validated_data):
-        """Update a user, setting the password correctly and return it."""
-        password = validated_data.pop('password', None)
-        user = super().update(instance, validated_data)
-
-        if password:
-            user.set_password(password)
-            user.save()
-
-        return user
